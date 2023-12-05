4a385063315de89b39dce31388ac7a7d5b6424e0
Adel Matias <adelmatias110@gmail.com>
2023-12-05 15:18:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.querySelector('form').addEventListener('submit' , checkGrade);
+
+const gradeA = 90;
+const gradeB = 80;
+const gradeC = 70;
+const gradeD = 60;
+
+function checkGrade(e) {
+    e.preventDefault();
+
+    let myGrade = document.querySelector('input').value;
+
+    let message = `<ul>`;
+
+    console.log("RUNNING..." +myGrade);
+    if (myGrade >=90) {
+        message = 'You got an A';
+    }
+
+    if (myGrade > 79 && myGrade < 90) {
+        message = 'You got a B';
+    }
+
+    if (myGrade >69 && myGrade < 80) {
+        message = 'You got a C';
+    }
+
+    if (myGrade >59 && myGrade < 70){
+        message = 'You got a D';
+    }
+
+    if (myGrade < 60) {
+        message = 'You got a F, study harder next time';
+    }
+    message += `</ul>` 
+
+    document.querySelector('#output').innerHTML = message;
+}
